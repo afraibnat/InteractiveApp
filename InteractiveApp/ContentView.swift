b4ff1edae6fdf230a8398b0ca22c262900d09821
Afra Ibnat <afraibnat91@gmail.com>
2024-06-14 09:57:28 -0500

    Initial Commit

---
+++ InteractiveApp/ContentView.swift
@@ -0,0 +1,39 @@
+//
+//  ContentView.swift
+//  InteractiveApp
+//
+//  Created by Afra Ibnat on 6/7/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    
+    @State private var name = ""
+    @State private var textTitle = "What is your name?"
+    
+    var body: some View {
+        VStack {
+            Text(textTitle)
+                .font(.title)
+            
+            TextField("Type here!", text: $name)
+                .multilineTextAlignment(.center)
+                .font(.title)
+                .border(Color.gray, width: 1)
+            
+            Button("Submit Name") {
+                //print(name)
+                textTitle = "Welcome, \(name)!"
+            }
+            .font(.title2)
+            .buttonStyle(.borderedProminent)
+            .tint(.purple)
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
